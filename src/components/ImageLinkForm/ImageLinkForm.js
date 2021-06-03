c4ea6cb92ab4a33cc98855df3e69091f628d7df9
Primus023008 <vasuanush@gmail.com>
2021-06-04 00:30:07 +0530

    responsive?

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -3,15 +3,18 @@ import './ImageLinkForm.css';
 
 const ImageLinkForm = () => {
     return (
-        <div className="searchGroup1">
+        <div className="outer">
             <div >
                 <p>Enter Image Link Below to Detect Faces</p>
             </div>
-            <div className="searchGroup2">
-                <input type='text' 
-                value="Enter Image Link"
-                className='input'/>
-                <button className="searchButton">Detect!</button>
+            <div className="center">
+                <div className="inner center">
+                    <input type='text' 
+                    placeholder="Enter Image Link"
+                    className='input'/>
+                    <button className="searchButton">Detect!</button>
+                </div>
+
             </div>
         </div>
         
