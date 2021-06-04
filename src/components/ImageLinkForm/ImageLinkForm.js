d354371bc9672c62e9ff299bddd5077b501c550c
Primus023008 <vasuanush@gmail.com>
2021-06-04 11:43:00 +0530

    ok

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,21 +5,9 @@ const ImageLinkForm = () => {
     return (
         <div className="outer">
             
-<<<<<<< HEAD
-                <p>Enter Image Link Below to Detect Faces</p>
-            
-            <div className="center">
-                <div className="inner center">
-                    <input type='text' 
-                    placeholder="Enter Image Link"
-                    className='input'/>
-                    <button className="searchButton">Detect!</button>
-                </div>
-=======
             <p>Enter Image Link Below to Detect Faces</p>
->>>>>>> 3e351ab49208009616510f9c50289bdd82e9493f
 
-            <div className="center">
+            <div className="inner">
                 <input type='text' 
                 placeholder="Enter Image Link"
                 className='input'/>
